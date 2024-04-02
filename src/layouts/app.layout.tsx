2f4ea2c1c0a5357a54eea3424f3c05a6a0869b47
Clevane Villareal <clevanevillareal1999@gmail.com>
2024-04-02 11:13:30 +0800

    feat: add shadcn setup (#3)

--- src/layouts/app.layout.tsx
+++ src/layouts/app.layout.tsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 export const AppLayout = () => {
   return (
     <main>
-      <h1>App Layout</h1>
       <Outlet />
     </main>
   );
--- src/layouts/auth.layout.tsx
+++ src/layouts/auth.layout.tsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 export const AuthLayout = () => {
   return (
     <main>
-      <h1>Auth Layout</h1>
       <Outlet />
     </main>
   );
